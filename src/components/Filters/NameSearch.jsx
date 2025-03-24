d2f87d5f6a0f80e080417fb3bd80e519beaa566f
dimitrisAvgoustopoulos <106197029+dimitrisAvgoustopoulos@users.noreply.github.com>
2025-03-24 11:20:05 +0200

    Update NameSearch.jsx

--- src/components/Filters/NameSearch.jsx
+++ src/components/Filters/NameSearch.jsx
@@ -10,8 +10,8 @@ export default function NameSearch({ onNameSearch }){
     
 
     return (
-        <div className="text-center my-24 lg:mb-36 xl:mb-64">
-            <input className='rounded-full w-2/3 mx-auto p-2 text-center text-xl font-semibold focus:outline-none sm:w-2/6 xl:w-1/6'
+        <div className="text-center lg:mb-36 xl:mb-64">
+            <input className='rounded-full w-5/6 mx-auto p-2 text-center text-xl font-semibold focus:outline-none sm:w-full'
                 type="text"
                 placeholder="Search Pokémon"
                 value={filter}
