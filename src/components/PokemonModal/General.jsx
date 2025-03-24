b3e7019486fc2154595ed1fc6e13226b1d0c5314
dimitrisAvgoustopoulos <106197029+dimitrisAvgoustopoulos@users.noreply.github.com>
2025-03-24 11:22:21 +0200

    Update General.jsx

--- src/components/PokemonModal/General.jsx
+++ src/components/PokemonModal/General.jsx
@@ -1,4 +1,5 @@
 import typeColors from "../../typeColors.json";
+import Loader from "../Loader";
 
 export default function General({pokemon}) {
 
@@ -9,9 +10,12 @@ export default function General({pokemon}) {
 
     return(
         <div className="flex flex-col items-center justify-center gap-2">
-            <img src={pokemon.sprites?.other?.home.front_default} alt={pokemon.name} className="w-2/3 max-w-md cursor-pointer transition-all duration-200 active:scale-110" 
-                onMouseDown={()=>{playCry()}}
-            />
+            {pokemon.sprites?.other?.home.front_default ? 
+                <img src={pokemon.sprites?.other?.home.front_default} alt={pokemon.name} className="w-2/3 max-w-md cursor-pointer transition-all duration-200 active:scale-110" 
+                    onMouseDown={()=>{playCry()}}
+                />
+                : <Loader className="mt-72"/>
+            }
             <ul className="flex flex-row items-center gap-2 mt-2 sm:gap-4 sm:mt-4">
             {pokemon.types?.map((type) => (
                 <li key={type.type.name} className="text-white text-lg font-semibold p-1 w-20 text-center rounded-xl sm:text-3xl sm:w-40" style={{ backgroundColor: typeColors[type.type.name] }}>
