0e9eb78ed35497cc6c6c444c633ede51d7e47305
dimitrisAvgoustopoulos <106197029+dimitrisAvgoustopoulos@users.noreply.github.com>
2025-03-24 11:21:40 +0200

    Update PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useRef } from "react"
 import { pokemonInfo } from "../fetch-api-data"
+
 import PokeInfoModal from "./PokemonModal/PokeInfoModal";
+import Loader from "./Loader";
 
 export default function PokemonCard({ pokeUrl }) {
 
@@ -30,14 +32,19 @@ export default function PokemonCard({ pokeUrl }) {
     <>
       <div className='w-full h-36 md:h-44 lg:h-52 xl:h-64' key={pokeUrl}>
 
-        <div className='shadow-md shadow-slate-400 rounded-xl cursor-pointer'>
+        <div className='shadow-md shadow-slate-400 rounded-xl cursor-pointer' onClick={() => handleOpenPokemon()}>
             <div className="flex justify-center h-20 rounded-t-xl bg-gradient-to-tr from-slate-300 to-slate-700 p-2 relative">
-              <img className={`w-full absolute ${positioning} ${smPositioning} ${mdPositioning} ${lgPositioning} ${xlPositioning} ${twoxlPositioning} hover:scale-125 transition-all duration-300`}
-                src={pokemon.sprites?.other?.home.front_default} 
-                alt={pokemon.name}
-                loading="lazy"
-                onClick={() => handleOpenPokemon()}
-              />
+              {pokemon.sprites?.other?.home.front_default ? (
+                    <img className={`w-full absolute ${positioning} ${smPositioning} ${mdPositioning} ${lgPositioning} ${xlPositioning} ${twoxlPositioning} hover:scale-125 transition-all duration-300`}
+                    src={pokemon.sprites?.other?.home.front_default }
+                    alt={pokemon.name}
+                    loading="lazy"
+                    onClick={() => handleOpenPokemon()}
+                  />
+                ) : (
+                    <Loader />
+                )
+              }
             </div>
             <div className="flex justify-between text-xs font-bold rounded-b-xl bg-gray-100 p-2">
               <p>#{pokemon.id}</p>
